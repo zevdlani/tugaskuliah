d0de6cf7997c20c637132172b1f9723b2e254e2d
Zevdlani <zevdlani@gmail.com>
2019-04-29 23:03:30 +0700

    Update video.php

--- video.php
+++ video.php
@@ -5,7 +5,7 @@
       $resource = opendir("upload/vid");
       while (($entry = readdir($resource)) !== false){
         if($entry != "." && $entry != ".."){
-        echo '<video width="100%" controls><source src="/tugaskuliah/upload/vid/'.$entry.'"type="video/mp4">Your browser does not support the video player</video><br><br><br>';
+        echo '<video width="100%" controls><source src="/upload/vid/'.$entry.'"type="video/mp4">Your browser does not support the video player</video><br><br><br>';
         }
       }
     ?>
